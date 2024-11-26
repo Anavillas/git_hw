f31798c2bb6abf23829ecb15f3d781212cef1861
Ana Villas <anaprimo@edu.unifil.br>
2024-11-25 23:08:19 -0300

    Corrigir mensagem da função imprimir_mensagem

--- HL.py
+++ HL.py
@@ -2,8 +2,8 @@ print("Hello World!")
 
 
 def imprimir_mensagem():
-    print("Mensagem personalizada!")
+    print("Mensagem corrigida e mais clara!")
 
 if __name__ == "__main__":
     print("Hello, World!")
-    imprimir_mensagem()
+    imprimir_mensagem()  # Chamando a nova função
