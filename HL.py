7d10501aa6abcb4f8af942ebcd82587a7857b683
Ana Villas <anaprimo@edu.unifil.br>
2024-11-28 21:25:33 -0300

    Adicionar função soma para somar dois números

--- HL.py
+++ HL.py
@@ -1,9 +1,11 @@
-print("Hello World!")
-
-
 def imprimir_mensagem():
     print("Mensagem corrigida e mais clara!")
 
+def soma(a, b):
+    return a + b
+
 if __name__ == "__main__":
     print("Hello, World!")
-    imprimir_mensagem()  # Chamando a nova função
+    imprimir_mensagem()
+    resultado = soma(3, 5)
+    print(f"A soma de 3 e 5 é: {resultado}")
